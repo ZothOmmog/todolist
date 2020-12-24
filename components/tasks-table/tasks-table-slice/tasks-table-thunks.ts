d8c50f9665d85b73b3f16c1c0cecf07df71b6f48
ZothOmmog <abpstd@gmail.com>
2020-12-24 20:31:29 +0500

    Реализована санка для получения агрегированной информации по таскам

--- components/tasks-table/tasks-table-slice/tasks-table-thunks.ts
+++ components/tasks-table/tasks-table-slice/tasks-table-thunks.ts
@@ -1,14 +1,27 @@
 import { getAgreateTasksData } from "../../../api/tasks-table-api";
 import { AppThunk } from "../../../redux";
+import { errorModalActions } from "../../error-modal";
+import { IAgregateTaskInfo } from "../tasks-table-types";
 import { tasksTableActions } from "./tasks-table-slice";
 
-const tasksTableThunks = {
+export const tasksTableThunks = {
     fetchAll: (): AppThunk => async dispatch => {
+        let isError = false;
+        let tasksInfo: IAgregateTaskInfo[];
+
         try {
-            const tasksInfo = await getAgreateTasksData();
+            tasksInfo = await getAgreateTasksData();
         }
         catch(e) {
-            
+            dispatch(errorModalActions.showError({ 
+                title: 'Ошибка при запросе агрегированной информации по таскам',
+                message: e.message
+            }));
+            isError = true;
         }
+
+        if (isError) return;
+
+        dispatch(tasksTableActions.setAll(tasksInfo));
     }
 };
