62356f52e2be11db269d78ffc9a26c3967ff05ab
ZothOmmog <abpstd@gmail.com>
2020-12-24 23:17:38 +0500

    Корректировка логики redux для работы с новым хуком

--- components/tasks-table/tasks-table-slice/tasks-table-thunks.ts
+++ components/tasks-table/tasks-table-slice/tasks-table-thunks.ts
@@ -1,27 +1,9 @@
 import { getAgreateTasksData } from "../../../api/tasks-table-api";
 import { AppThunk } from "../../../redux";
-import { errorModalActions } from "../../error-modal";
-import { IAgregateTaskInfo } from "../tasks-table-types";
 import { tasksTableActions } from "./tasks-table-slice";
 
 export const tasksTableThunks = {
     fetchAll: (): AppThunk => async dispatch => {
-        let isError = false;
-        let tasksInfo: IAgregateTaskInfo[];
-
-        try {
-            tasksInfo = await getAgreateTasksData();
-        }
-        catch(e) {
-            dispatch(errorModalActions.showError({ 
-                title: 'Ошибка при запросе агрегированной информации по таскам',
-                message: e.message
-            }));
-            isError = true;
-        }
-
-        if (isError) return;
-
-        dispatch(tasksTableActions.setAll(tasksInfo));
+        dispatch(tasksTableActions.setAll(await getAgreateTasksData()));
     }
 };
--- redux/index.ts
+++ redux/index.ts
@@ -13,6 +13,6 @@ export const store = configureStore({
 
 
 export type RootState = ReturnType<typeof store.getState>;
-type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
