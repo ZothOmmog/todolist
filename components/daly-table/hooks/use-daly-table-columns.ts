7166b872a8edf404f6b35d00d2f979d730a01b38
ZothOmmog <abpstd@gmail.com>
2020-12-26 16:26:58 +0500

    Убраны минуты, типизированы столбцы, исправлено выравнивание

--- components/daly-table/hooks/use-daly-table-columns.ts
+++ components/daly-table/hooks/use-daly-table-columns.ts
@@ -1,31 +1,37 @@
+import { ColumnsType } from 'antd/lib/table';
 import { format } from 'date-fns';
 import { IDalyTableItemTask } from '../daly-table-slice';
 
 export const useDalyTableColumns = () => {
-    const renderTimeFromUtc = (dateStrUtc: string) => format(new Date(dateStrUtc), 'HH:mm:ss');
-    return [
+    const renderTimeFromUtc = (dateStrUtc: string) => format(new Date(dateStrUtc), 'HH:mm');
+    const columns: ColumnsType<IDalyTableItemTask> = [
         {
             title: 'Дата',
             dataIndex: 'date',
-            render: (dateStrUtc: string) => format(new Date(dateStrUtc), 'dd.MM.yyyy')
+            render: (dateStrUtc: string) => format(new Date(dateStrUtc), 'dd.MM.yyyy'),
+            align: 'right'
         },
         {
             title: 'Номер таска',
-            dataIndex: 'keyTask'
+            dataIndex: 'keyTask',
+            align: 'right'
         },
         {
             title: 'Время начала',
             dataIndex: 'timeStart',
-            render: renderTimeFromUtc
+            render: renderTimeFromUtc,
+            align: 'right'
         },
         {
             title: 'Время конца',
             dataIndex: 'timeEnd',
-            render: renderTimeFromUtc
+            render: renderTimeFromUtc,
+            align: 'right'
         },
         {
             title: 'Описание',
             dataIndex: 'desctiption'
         }
-    ]
+    ];
+    return columns;
 };
