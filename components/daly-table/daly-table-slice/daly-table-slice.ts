957f9600f3d4744f5879d265ac12895dcdea1b00
ZothOmmog <abpstd@gmail.com>
2021-01-19 16:42:28 +0500

    Убран лишний код в редьюсере

--- components/daly-table/daly-table-slice/daly-table-slice.ts
+++ components/daly-table/daly-table-slice/daly-table-slice.ts
@@ -1,32 +1,11 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
-import Item from 'antd/lib/list/Item';
-import { addHours, compareAsc, startOfDay } from 'date-fns';
+import { compareAsc } from 'date-fns';
 import { dalyTableApi } from '../../../api/daly-table-api';
 import { fetchErrors, isLoading } from '../../../common-types';
 import { normalizeTodosFromDb } from '../../../helpers/normalize-todos-from-db';
 import { RootState } from '../../../redux';
 import { errorModalActions } from '../../error-modal';
 import * as types from './daly-table-slice-types';
-
-let MOCK_ID = 2;
-const MOCK_DALY_ITEMS = [
-    {
-        key: 0,
-        date: startOfDay(new Date()).toUTCString(),
-        keyTask: 228,
-        desctiption: 'Какое-то описание для действий по таску 228',
-        timeStart: addHours(new Date(), -5).toUTCString(),
-        timeEnd: addHours(new Date(), -3).toUTCString()
-    },
-    {
-        key: 1,
-        date: startOfDay(new Date()).toUTCString(),
-        keyTask: 1488,
-        desctiption: 'Какое-то описание для действий по таску 1488',
-        timeStart: addHours(new Date(), -3).toUTCString(),
-        timeEnd: addHours(new Date(), -2).toUTCString()
-    }
-];
 
 const SLICE_NAME = 'dalyTable';
 
@@ -39,27 +18,6 @@ const adapter = createEntityAdapter<types.IDalyTableItemTask>({
 });
 
 const thunks = {
-    fetchDalyItemAdded: createAsyncThunk<
-        types.IDalyTableItemTask[],
-        types.IDalyItemForFetch,
-        {
-            rejectValue: fetchErrors,
-            state: RootState
-        }
-    >(
-        `${SLICE_NAME}/fetchDalyItemAdded`,
-        async (dalyItem, thunkAPI) => {
-            try {
-                const { cancelAddItem } = thunkAPI.getState().dalyTable;
-                if (cancelAddItem) return;
-
-                const result = await dalyTableApi.addTask(dalyItem);
-                return normalizeTodosFromDb(result);
-            }
-            catch(e) {
-            }
-        }
-    ),
     fetchEditItem: createAsyncThunk<
         types.IDalyTableItemTask[],
         types.IDalyTableItemTaskDB,
@@ -71,9 +29,6 @@ const thunks = {
         `${SLICE_NAME}/fetchEditItem`,
         async (editedItem, thunkAPI) => {
             try {
-                const { cancelEditItem } = thunkAPI.getState().dalyTable;
-                if (cancelEditItem) return;
-
                 const result = await dalyTableApi.updateTask(editedItem);
                 return normalizeTodosFromDb(result);
             }
@@ -91,28 +46,13 @@ const thunks = {
 const { actions, reducer } = createSlice({
     name: SLICE_NAME,
     initialState: adapter.getInitialState({
-        isLoading: isLoading.initial,
-        cancelAddItem: false,
-        cancelEditItem: false
+        isLoading: isLoading.initial
     }),
     reducers: {
         dalyItemAdded: adapter.addOne,
-        cancelAddItem(state) {
-            state.cancelAddItem = true;
-        },
         dalyItemsChanged: adapter.setAll
     },
     extraReducers: builder => {
-        builder.addCase(thunks.fetchDalyItemAdded.pending, (state) => {
-            state.cancelAddItem = false;
-        });
-        builder.addCase(thunks.fetchDalyItemAdded.fulfilled, (state, action) => {
-            adapter.setAll(state, action.payload);
-        });
-
-        builder.addCase(thunks.fetchEditItem.pending, (state) => {
-            state.cancelEditItem = false;
-        });
         builder.addCase(thunks.fetchEditItem.fulfilled, (state, action) => {
             adapter.setAll(state, action.payload);
         });
