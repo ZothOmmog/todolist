ca77d269f24ed214651344793135b72ced606f54
ZothOmmog <abpstd@gmail.com>
2021-01-19 16:40:58 +0500

    Добавлен хук для вызова санок через него, предоставляет возможность работать через промисы.

---
+++ components/use-thunk.ts
@@ -0,0 +1,39 @@
+import { useCallback, useState } from "react";
+import { AppThunk, useAppDispatch } from "../redux";
+import { errorModalActions } from "./error-modal";
+
+interface IOptions<T> {
+    thunkCreator: (args: T) => AppThunk,
+    titleError?: string
+}
+
+export enum ResultsThunkExecute {
+    success,
+    error
+}
+
+export function useThunk<T> (options: IOptions<T>): [(args: T) => Promise<ResultsThunkExecute>, boolean] {
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const dispatch = useAppDispatch();
+
+    const asyncHandlerMemo = useCallback(async (args: T) => {
+        setIsLoading(true);
+        try {
+            await dispatch(options.thunkCreator(args));
+            return ResultsThunkExecute.success;
+        }
+        catch(e) {
+            dispatch(errorModalActions.showError({ 
+                title: options.titleError ?? '',
+                message: e.message
+            }));
+            return ResultsThunkExecute.error;
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }, [options.thunkCreator, dispatch]);
+
+    return [asyncHandlerMemo, isLoading];
+}
